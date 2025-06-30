27781edb3e5e44b07d65055f7185ad519cd3443a
plumstar3 <marplumstar@gmail.com>
2025-06-30 14:42:50 +0900

    初期値設定可能

--- nba_score_model/config.py
+++ nba_score_model/config.py
@@ -10,10 +10,10 @@ LIMIT_ROWS = 225109
 # --- 分析対象のイベントカテゴリ ---
 # これらの名前を基に、特徴量を作成
 EVENT_CATEGORIES = [
-    # '2pt_success',
-    # '3pt_success',
+    '2pt_success',
+    '3pt_success',
     'shot_miss',
-    # 'ft_success',
+    'ft_success',
     'rebound',
     'unforced_turnover',
     'steal',
