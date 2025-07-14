50e077fd7f6d584c9ff1fd3d713252b031a447ff
plumstar3 <marplumstar@gmail.com>
2025-07-14 11:19:59 +0900

    イベント細分化

--- nba_score_model/config.py
+++ nba_score_model/config.py
@@ -13,10 +13,10 @@ LIMIT_ROWS = 225109
 # --- 分析対象のイベントカテゴリ ---
 # これらの名前を基に、特徴量を作成
 EVENT_CATEGORIES = [
-    '2pt_success',
-    '3pt_success',
+    #'2pt_success',
+    #'3pt_success',
     'shot_miss_unblocked', # ブロックされなかったシュート失敗
-    'ft_success',
+    #'ft_success',
     'ft_miss',            
     'rebound_off',        
     'rebound_def',         
